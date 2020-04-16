7f8d40ce847c3fd408a0c40194511b107107e380
gal <gal.toledano@mail.huji.ac.il>
2020-04-16 19:50:49 +0300

    done

--- ex1.py
+++ ex1.py
@@ -4,7 +4,8 @@ from Bio import pairwise2
 import Bio.Seq as seq
 
 
-def q9_1(id1, chain1, id2, chain2):
+# BONUS 9.2 IMPLEMENTED
+def align_structs(id1, chain1, id2, chain2):
     """
     the main function. gets the ids and the chain's names and finds the alignment with the best RMSD.
     prints the best RMSD, and saving the alignments file in cif format
@@ -21,23 +22,10 @@ def q9_1(id1, chain1, id2, chain2):
     struct1 = parser.get_structure("p1", protein1)
     struct2 = parser.get_structure("p2", protein2)
 
-    # ppbils = pdb.CaPPBuilder()
-    # pp_lst1, pp_lst2 = seq.Seq(""), seq.Seq("")
-    # for pp in ppbils.build_peptides(struct1):
-    #     pp_lst1 += pp.get_sequence()
-    # for pp in ppbils.build_peptides(struct2):
-    #     pp_lst2 += pp.get_sequence()
-    # alignments = pairwise2.align.globalxx(pp_lst1, pp_lst2)
-    # print(alignments)
-    # ignore_inx1 = [i for i in range(len(alignments[0][0])) if alignments[0][0][i] == "-"]
-    # ignore_inx2 = [i for i in range(len(alignments[0][1])) if alignments[0][1][i] == "-"]
-
-    # creating atoms to align lists
     atoms1 = create_atoms_list(struct1, chain1, [])
     atoms2 = create_atoms_list(struct2, chain2, [])
-    # if len(atoms1) != len(atoms2):
-    #     atoms2 = [atoms2[i] for i in range(len(atoms2)) if i not in ignore_inx1]
-    #     atoms1 = [atoms1[i] for i in range(len(atoms1)) if i not in ignore_inx2]
+    if len(atoms1) != len(atoms2):
+        atoms1, atoms2 = bonus_9_2(atoms2, chain1, chain2, struct1, struct2)
 
     # making the align
     super_imposer = pdb.Superimposer()
@@ -49,6 +37,42 @@ def q9_1(id1, chain1, id2, chain2):
     saving_file(id1, struct1)
     saving_file(id2, struct2)
 
+
+def bonus_9_2(a2, chain1, chain2, struct1, struct2):
+    ppbils = pdb.CaPPBuilder()
+    p1 = []
+    p2 = []
+    for pp in ppbils.build_peptides(struct1):
+        for r in pp:
+            if r.get_parent().get_id() == chain1:
+                p1.append(r)
+    for pp in ppbils.build_peptides(struct2):
+        for r in pp:
+            if r.get_parent().get_id() == chain2:
+                p2.append(r)
+    p1 = pdb.Polypeptide.Polypeptide(p1)
+    p2 = pdb.Polypeptide.Polypeptide(p2)
+    pp_lst1 = p1.get_sequence()
+    a1 = p1.get_ca_list()
+    pp_lst2 = p2.get_sequence()
+    a2 = p2.get_ca_list()
+    alignments = pairwise2.align.globalxx(pp_lst1, pp_lst2)
+    a1 = choose_chain_atoms(a1, chain1)
+    print(alignments)
+    ignore_inx1 = [i for i in range(len(alignments[0][0])) if alignments[0][0][i] == "-"]
+    ignore_inx2 = [i for i in range(len(alignments[0][1])) if alignments[0][1][i] == "-"]
+    if len(a1) != len(a2):
+        a2 = [a2[i] for i in range(len(a2)) if i not in ignore_inx1]
+        a1 = [a1[i] for i in range(len(a1)) if i not in ignore_inx2]
+    return a1, a2
+
+
+def choose_chain_atoms(atoms, chain):
+    lst = []
+    for atom in atoms:
+        if atom.get_parent().get_parent().get_id() == chain:
+            lst.append(atom)
+    return lst
 
 def saving_file(id, struct):
     """
@@ -72,7 +96,7 @@ def create_atoms_list(struct, chain_name, ignore_inx):
     for model in struct:
         for chain in model:
             if chain.get_id() == chain_name:
-                lst += [atom for atom in chain.get_atoms() if atom.get_name() == 'CA']
+                lst += [atom for atom in chain.get_atoms() if atom.get_id() == 'CA']
                 # for i in range(len(atoms)):
                 #     if atoms[i].get_name() == 'CA' and i not in ignore_inx:
                 #         lst.append(atoms[i])
@@ -83,7 +107,7 @@ if __name__ == '__main__':
     # prints Error in case of invalid number of arguments
     if len(sys.argv) != 5:
         print("Error: invalid number of arguments")
-    q9_1(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    align_structs(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
 
 
