9ee5056d51d128e22b59339ffeef0d384e16cd3e
gal <gal.toledano@mail.huji.ac.il>
2020-04-16 20:16:11 +0300

    done for real ya

--- ex1.py
+++ ex1.py
@@ -22,10 +22,11 @@ def align_structs(id1, chain1, id2, chain2):
     struct1 = parser.get_structure("p1", protein1)
     struct2 = parser.get_structure("p2", protein2)
 
-    atoms1 = create_atoms_list(struct1, chain1, [])
-    atoms2 = create_atoms_list(struct2, chain2, [])
+    # creating a lists of CA atoms to align
+    atoms1 = create_atoms_list(struct1, chain1)
+    atoms2 = create_atoms_list(struct2, chain2)
     if len(atoms1) != len(atoms2):
-        atoms1, atoms2 = bonus_9_2(atoms2, chain1, chain2, struct1, struct2)
+        atoms1, atoms2 = bonus_9_2(chain1, chain2, struct1, struct2)
 
     # making the align
     super_imposer = pdb.Superimposer()
@@ -33,46 +34,59 @@ def align_structs(id1, chain1, id2, chain2):
     super_imposer.apply(struct2[0].get_atoms())
     print(super_imposer.rms)
 
-    # saving to file
+    # saving the aligned structure to files
     saving_file(id1, struct1)
     saving_file(id2, struct2)
 
 
-def bonus_9_2(a2, chain1, chain2, struct1, struct2):
+def bonus_9_2(chain1, chain2, struct1, struct2):
+    """
+    handling the case that the number of atoms is different by align the amino-acids and remove the unnecessary atoms.
+    :param chain1: the first protein's chain
+    :param chain2: the second protein's chain
+    :param struct1: the first protein
+    :param struct2: the second protein
+    :return: filtered lists of atoms to align
+    """
     ppbils = pdb.CaPPBuilder()
-    p1 = []
-    p2 = []
-    for pp in ppbils.build_peptides(struct1):
-        for r in pp:
-            if r.get_parent().get_id() == chain1:
-                p1.append(r)
-    for pp in ppbils.build_peptides(struct2):
-        for r in pp:
-            if r.get_parent().get_id() == chain2:
-                p2.append(r)
-    p1 = pdb.Polypeptide.Polypeptide(p1)
-    p2 = pdb.Polypeptide.Polypeptide(p2)
-    pp_lst1 = p1.get_sequence()
-    a1 = p1.get_ca_list()
-    pp_lst2 = p2.get_sequence()
-    a2 = p2.get_ca_list()
-    alignments = pairwise2.align.globalxx(pp_lst1, pp_lst2)
-    a1 = choose_chain_atoms(a1, chain1)
-    print(alignments)
+    peptide1, peptide2 = [], []
+    filter_peptide_by_chain(chain1, peptide1, ppbils, struct1)
+    filter_peptide_by_chain(chain2, peptide2, ppbils, struct2)
+
+    # converting list to peptide
+    peptide1 = pdb.Polypeptide.Polypeptide(peptide1)
+    peptide2 = pdb.Polypeptide.Polypeptide(peptide2)
+
+    # converting peptide to sequence and CA list
+    seq1 = peptide1.get_sequence()
+    atoms1 = peptide1.get_ca_list()
+    seq2 = peptide2.get_sequence()
+    atoms2 = peptide2.get_ca_list()
+
+    # align the sequences
+    alignments = pairwise2.align.globalxx(seq1, seq2)
+
+    # filter the atoms lists
     ignore_inx1 = [i for i in range(len(alignments[0][0])) if alignments[0][0][i] == "-"]
     ignore_inx2 = [i for i in range(len(alignments[0][1])) if alignments[0][1][i] == "-"]
-    if len(a1) != len(a2):
-        a2 = [a2[i] for i in range(len(a2)) if i not in ignore_inx1]
-        a1 = [a1[i] for i in range(len(a1)) if i not in ignore_inx2]
-    return a1, a2
+    atoms2 = [atoms2[i] for i in range(len(atoms2)) if i not in ignore_inx1]
+    atoms1 = [atoms1[i] for i in range(len(atoms1)) if i not in ignore_inx2]
+    return atoms1, atoms2
 
 
-def choose_chain_atoms(atoms, chain):
-    lst = []
-    for atom in atoms:
-        if atom.get_parent().get_parent().get_id() == chain:
-            lst.append(atom)
-    return lst
+def filter_peptide_by_chain(chain, peptide, ppbils, struct):
+    """
+    this function filters the peptide atoms by given chain
+    :param chain: the given chain
+    :param peptide: the filtered list
+    :param ppbils: the peptide to filter
+    :param struct: the structure
+    """
+    for pp in ppbils.build_peptides(struct):
+        for r in pp:
+            if r.get_parent().get_id() == chain:
+                peptide.append(r)
+
 
 def saving_file(id, struct):
     """
@@ -85,7 +99,7 @@ def saving_file(id, struct):
     io.save("{0}.cif".format(id))
 
 
-def create_atoms_list(struct, chain_name, ignore_inx):
+def create_atoms_list(struct, chain_name):
     """
     creates a list with CA atoms to align from the given structure at the given chain
     :param struct: the structure
@@ -97,9 +111,6 @@ def create_atoms_list(struct, chain_name, ignore_inx):
         for chain in model:
             if chain.get_id() == chain_name:
                 lst += [atom for atom in chain.get_atoms() if atom.get_id() == 'CA']
-                # for i in range(len(atoms)):
-                #     if atoms[i].get_name() == 'CA' and i not in ignore_inx:
-                #         lst.append(atoms[i])
     return lst
 
 
